53cf4f389979d6840d0fe40cc5c8e3d9d67bbcff
Smallwood, Kalil <ksmallwood@arete.com>
2024-06-27 17:45:52 -0400

    Added scrape/parse_team_rankings

--- web_scraping.py
+++ web_scraping.py
@@ -50,9 +50,7 @@ def parse_mvp_data():
 
 def scrape_player_stats():
     url_start = 'https://www.basketball-reference.com/leagues/NBA_{}_per_game.html'
-    headers = {
-        'User-agent':"Mozilla/5.0 (Windows NT 6.1; rv:109.0) Gecko/20100101 Firefox/113.0",
-    }
+
     driver = webdriver.Chrome(executable_path='/path/to/executable')
     years = list(range(1991,2025))
 
@@ -84,9 +82,49 @@ def parse_player_stats():
     players = pd.concat(dfs)
     players.to_csv('players.csv')
 
-
+#######################################  Team Data  #######################################
         
+def scrape_team_rankings():
+    url_start = 'https://www.basketball-reference.com/leagues/NBA_{}_standings.html'
+    headers = {
+        'User-agent':"Mozilla/5.0 (Windows NT 6.1; rv:109.0) Gecko/20100101 Firefox/113.0",
+    }
+    years = list(range(1991,2025))
+    for year in years:
+        url = url_start.format(year)
+        response = requests.get(url,headers=headers,verify=False)
 
+        with open('team/{}.html'.format(year), 'w+',encoding='utf-8') as f:
+            f.write(response.data)
+
+def parse_team_rankings():
+    dfs = []
+    years = list(range(1991,2025))
+
+    for year in years:
+        with open('team/{}.html'.format(year),encoding='utf-8') as f:
+            page = f.read()
+
+        soup = BeautifulSoup(page, 'html.parser')
+        soup.find('tr', class_ = "thead").decompose()
+        team_table = soup.find_all(id='divs_standings_E')
+        team = pd.read_html(str(team_table))[0]
+        team["Year"] = year
+        team['Team'] = team['Eastern Conference']  
+        del team['Eastern Conference'] 
+        dfs.append(team)
+
+        soup = BeautifulSoup(page, 'html.parser')
+        soup.find('tr', class_ = "thead").decompose()
+        team_table = soup.find_all(id='divs_standings_W')
+        team = pd.read_html(str(team_table))[0]
+        team["Year"] = year
+        team['Team'] = team['Western Conference']  
+        del team['Western Conference'] 
+        dfs.append(team)   
+
+    teams = pd.concat(dfs)
+    teams.to_csv('teams.csv')   
 
 
 
